50d0527cfbe802705a33f5d4fada411ac2f9cf0a
thaina-abibi <thainau.u@gmail.com>
2025-09-10 22:36:14 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -33,16 +33,6 @@ int main() {
         contador_rainha++;
     }   while(contador_rainha < casas_rainha);
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
 }
